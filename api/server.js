ef1556a252c830979c19b079ec17b61d7db08c49
Brandon Pérez <stivenbustamante46@gmail.com>
2020-05-19 15:53:37 -0500

    Quitar cors

--- api/server.js
+++ api/server.js
@@ -35,16 +35,16 @@ const allowedOrigins = [
   process.env.CORS_ORIGIN_CLIENT_PDN
 ]
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true)
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('Request origin host not allowed by CORS policy.'), false)
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}))
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true)
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       return callback(new Error('Request origin host not allowed by CORS policy.'), false)
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true
+// }))
 
 /** Creating and running API Server */
 const server = app.listen(process.env.SERVER_PORT, process.env.HOST, function () {
