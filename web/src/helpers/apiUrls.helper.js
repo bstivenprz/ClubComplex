d26032a4ec4d5e50ab4a4cce41dc6f893356e148
Brandon Pérez <stivenbustamante46@gmail.com>
2020-05-19 15:37:18 -0500

    Ajuste de restaurar cuenta

--- web/src/helpers/apiUrls.helper.js
+++ web/src/helpers/apiUrls.helper.js
@@ -8,6 +8,7 @@ export const API_PROJECTS_LIST = API_HOST + 'listProjects';
 export const API_PROJECTS = API_HOST + 'projects';
 
 export const API_USER_EDIT = API_HOST + 'users/edit';
+export const API_USER_RESTORE = API_HOST + 'users/restorePassword';
 
 export const API_SHOP_CHECKOUT = API_HOST + 'shop/checkout';
 
--- web/src/views/RestorePassword/index.js
+++ web/src/views/RestorePassword/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from 'axios';
 import { Link as RouterLink } from "react-router-dom";
 
 /** Material UI */
@@ -12,9 +13,14 @@ import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Alert from "@material-ui/lab/Alert";
+import Collapse from '@material-ui/core/Collapse';
 
 /** Components */
 import Footer from "../../components/secondary-footer";
+
+/** Services */
+import { API_USER_RESTORE } from '../../helpers/apiUrls.helper';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,6 +47,34 @@ const useStyles = makeStyles((theme) => ({
 
 export default function RestorePassword() {
   const classes = useStyles();
+  const [success, setSuccess] = useState(false);
+  const [emailRestore, setEmailRestore] = useState('');
+  const [ShowAlert, setShowAlert] = useState({
+    state: false,
+    type: "warning",
+    message: "",
+  });
+
+  const restoreService = (event) => {
+    event.preventDefault();
+    Axios.put(`${API_USER_RESTORE}/${emailRestore}`)
+    .then(response => {
+      setShowAlert({ ...ShowAlert, type: 'success', message: 'Tu cuenta ha sido restaurada. Revisa tu correo electrónico.' });
+      setSuccess(true);
+    })
+    .catch(error => {
+      if (error.response.status === 404) {
+        setShowAlert({ state: true, type: 'error', message: error.response.data.error });
+      }
+    })
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setShowAlert({ ...ShowAlert, state: false });
+  };
 
   return (
     <div>
@@ -60,38 +94,63 @@ export default function RestorePassword() {
               <Typography component="h1" variant="h5">
                 Restaurar cuenta
               </Typography>
-              <Typography component="p" variant="body1" align="center">
-                Ingresa tu correo electrónico para recuperar tu cuenta.
-              </Typography>
-              <form className={classes.form} noValidate>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Correo electrónico"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.submit}
-                >
-                  Recuperar cuenta
-                </Button>
-                <Grid container>
-                  <Grid item>
-                    <Link variant="body2" component={RouterLink} to="/">
-                      {"¿No tienes una cuenta? Regístrate Gratis."}
-                    </Link>
+              {success && (
+                <>
+                  <Alert onClose={handleClose} severity={ShowAlert.type}>
+                    {ShowAlert.message}
+                  </Alert>
+                  <Grid container>
+                    <Grid item>
+                      <Link variant="body2" component={RouterLink} to="/">
+                        {"¿No tienes una cuenta? Regístrate Gratis."}
+                      </Link>
+                    </Grid>
                   </Grid>
-                </Grid>
-              </form>
+                </>
+              )}
+              {!success && (
+                <>
+                  <Typography component="p" variant="body1" align="center">
+                    Ingresa tu correo electrónico para recuperar tu cuenta.
+                  </Typography>
+                  <Collapse in={ShowAlert.state}>
+                    <Alert open onClose={handleClose} severity={ShowAlert.type}>
+                      {ShowAlert.message}
+                    </Alert>
+                  </Collapse>
+                  <form className={classes.form} onSubmit={restoreService}>
+                    <TextField
+                      variant="outlined"
+                      margin="normal"
+                      id="email"
+                      label="Correo electrónico"
+                      name="email"
+                      autoComplete="email"
+                      value={emailRestore}
+                      onChange={e => setEmailRestore(e.target.value)}
+                      required
+                      fullWidth
+                      autoFocus
+                    />
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      className={classes.submit}
+                    >
+                      Recuperar cuenta
+                    </Button>
+                    <Grid container>
+                      <Grid item>
+                        <Link variant="body2" component={RouterLink} to="/">
+                          {"¿No tienes una cuenta? Regístrate Gratis."}
+                        </Link>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </>
+              )}
             </div>
           </Container>
         </Grid>
